51daea54f7c44418826b748a291455dd2fd2ddc9
pypymi <dr.mizoguchi@gmail.com>
2021-05-16 00:49:11 +0900

    chokudai_S001 A問題

---
+++ chokudai_S001/a/test.py
@@ -0,0 +1,46 @@
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_Sample_Input_1(self):
+        input = """5
+3 1 5 4 2"""
+        output = """5"""
+        self.assertIO(input, output)
+
+    def test_Sample_Input_2(self):
+        input = """6
+1 2 3 4 5 6"""
+        output = """6"""
+        self.assertIO(input, output)
+
+    def test_Sample_Input_3(self):
+        input = """7
+7 6 5 4 3 2 1"""
+        output = """7"""
+        self.assertIO(input, output)
+
+    def test_Sample_Input_4(self):
+        input = """20
+19 11 10 7 8 9 17 18 20 4 3 15 16 1 5 14 6 2 13 12"""
+        output = """20"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+def resolve():
+  n = int(input())
+  print(max(map(int,input().split())))
