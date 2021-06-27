0a7e9a5e10faf5c4c5a1cf7e10c895c23442cb31
pypymi <dr.mizoguchi@gmail.com>
2021-06-27 12:40:44 +0900

    abc207 A問題

---
+++ abc207/a/test.py
@@ -0,0 +1,38 @@
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """3 4 5"""
+        output = """9"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """6 6 6"""
+        output = """12"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """99 99 98"""
+        output = """198"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+def resolve():
+  abc = list(map(int,input().split()))
+  abc.sort()
+  print(abc[-1]+abc[-2])
