483a55b5d2837b27a6fd6d9ecf631ecd89af5879
pypymi <dr.mizoguchi@gmail.com>
2021-07-19 09:35:56 +0900

    abc210 B問題

---
+++ abc210/b/submission.py
@@ -0,0 +1,3 @@
+n = int(input())
+s = list(input())
+print("Takahashi") if s.index("1") % 2 == 0 else print("Aoki")
---
+++ abc210/b/test.py
@@ -0,0 +1,35 @@
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """5
+00101"""
+        output = """Takahashi"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """3
+010"""
+        output = """Aoki"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+def resolve():
+  n = int(input())
+  s = list(input())
+  print("Takahashi") if s.index("1")%2==0 else print("Aoki")
